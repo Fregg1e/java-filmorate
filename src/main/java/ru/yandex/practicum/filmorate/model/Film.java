a0e4bda4596dee31991f8a82357f50989d348084
Alexandr Shalaginov <alxshlgnv@yandex.ru>
2023-04-20 10:38:18 +0300

    feat: добавить классы Film и User

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,2 +1,14 @@
-package ru.yandex.practicum.filmorate.model;public class Film {
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Film {
+    private final Long id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private Integer duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,2 +1,14 @@
-package ru.yandex.practicum.filmorate.model;public class User {
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private final Long id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
 }
