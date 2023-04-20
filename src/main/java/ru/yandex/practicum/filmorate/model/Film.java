bb3a6e920d0c0c42ff08a8067ed0589b44013ac3
Alexandr Shalaginov <alxshlgnv@yandex.ru>
2023-04-20 14:45:44 +0300

    feat: добавить аннотации для валидации

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,14 +1,24 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+import ru.yandex.practicum.filmorate.util.validator.date.PastLocalDateConstrain;
 
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
 public class Film {
-    private final Long id;
+    private Long id;
+    @NotBlank(message = "Название не может быть пустым.")
     private String name;
+    @Size(max = 200, message = "Описание не может быть больше 200 символов.")
     private String description;
+    @PastLocalDateConstrain(current = "1895-12-28", message = "Дата релиза не может быть раньше 1895-12-28.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate releaseDate;
+    @Positive(message = "Продолжительность должна быть положительной.")
     private Integer duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,14 +1,25 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+import ru.yandex.practicum.filmorate.util.validator.string.NotSpacesConstrain;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
 @Data
 public class User {
-    private final Long id;
+    private Long id;
+    @NotBlank(message = "Email не может быть пустым.")
+    @Email(message = "Email не соответствует должному формату.")
     private String email;
+    @NotBlank(message = "Логин не может быть пустым.")
+    @NotSpacesConstrain(message = "Логин содержит пробелы.")
     private String login;
     private String name;
+    @PastOrPresent(message = "Дата рождения не может быть позже чем сегодня.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 }
