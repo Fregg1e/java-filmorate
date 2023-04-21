1aa736e46cc19bd33ffeaaa200c574244bc6dee2
Alexandr Shalaginov <alxshlgnv@yandex.ru>
2023-04-21 12:22:13 +0300

    fix: исправить message

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -18,9 +18,10 @@ public class Film {
     private String name;
     @Size(max = 200, message = "Описание не может быть больше 200 символов.")
     private String description;
-    @PastLocalDateConstrain(current = "1895-12-28", message = "Дата релиза не может быть раньше 1895-12-28.")
+    @PastLocalDateConstrain(current = "1895-12-28",
+            message = "Дата релиза не может быть раньше 1895-12-28 или равной null.")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate releaseDate;
-    @Positive(message = "Продолжительность должна быть положительной и не равна null.")
+    @Positive(message = "Продолжительность должна быть положительной.")
     private Integer duration;
 }
