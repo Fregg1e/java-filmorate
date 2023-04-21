8ee84d541688c0735fe9956d0e048db11dc9ef53
Alexandr Shalaginov <alxshlgnv@yandex.ru>
2023-04-21 10:30:27 +0300

    faet: создать генератор id

--- src/main/java/ru/yandex/practicum/filmorate/util/generator/IdGenerator.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/generator/IdGenerator.java
@@ -1,2 +1,9 @@
-package ru.yandex.practicum.filmorate.util.generator;public class IdGenerator {
+package ru.yandex.practicum.filmorate.util.generator;
+
+public class IdGenerator {
+    private Long id = 1L;
+
+    public Long generateId() {
+        return id++;
+    }
 }
