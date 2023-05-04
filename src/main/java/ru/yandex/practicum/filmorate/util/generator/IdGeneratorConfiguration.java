a7d5782642d550549f9396423224c86b466a75a0
Alexandr Shalaginov <alxshlgnv@yandex.ru>
2023-05-04 19:12:33 +0300

    feat: добавить конфиг для генератора id

--- src/main/java/ru/yandex/practicum/filmorate/util/generator/IdGeneratorConfiguration.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/generator/IdGeneratorConfiguration.java
@@ -1,2 +1,17 @@
-package ru.yandex.practicum.filmorate.util.generator;public class IdGeneratorConfiguration {
+package ru.yandex.practicum.filmorate.util.generator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class IdGeneratorConfiguration {
+    @Bean("userIdGenerator")
+    public IdGenerator userIdGenerator() {
+        return new IdGenerator();
+    }
+
+    @Bean("filmIdGenerator")
+    public IdGenerator filmIdGenerator() {
+        return new IdGenerator();
+    }
 }
