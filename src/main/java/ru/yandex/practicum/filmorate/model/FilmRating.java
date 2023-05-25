2f4b99482677c27b682b7956bb2888c8f1851cc1
Alexandr Shalaginov <alxshlgnv@yandex.ru>
2023-05-25 11:26:35 +0300

    feat: добавить поле ratingName для enum

--- src/main/java/ru/yandex/practicum/filmorate/model/FilmRating.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/FilmRating.java
@@ -1,9 +1,26 @@
 package ru.yandex.practicum.filmorate.model;
 
 public enum FilmRating {
-    G,
-    PG,
-    PG13,
-    R,
-    NC17
+    G ("G"),
+    PG ("PG"),
+    PG13 ("PG-13"),
+    R ("R"),
+    NC17 ("NC-17");
+
+    private String ratingName;
+
+    FilmRating(String ratingName) {
+        this.ratingName = ratingName;
+    }
+
+    public String getRatingName() {
+        return ratingName;
+    }
+
+    @Override
+    public String toString() {
+        return "FilmRating{" +
+                "ratingName='" + ratingName + '\'' +
+                '}';
+    }
 }
